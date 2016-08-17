e282f43b40b90d92490c27c0d0eb29c7ed6449d8
Manuel Elaraj <melaraj@gmail.com>
2016-08-17 15:25:39 -0400

    support for $KONG_CONFIG environment variable.. base 64 encoded

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -6,4 +6,9 @@ if [ -n "$DATABASE" ]; then
   echo -e '\ndatabase: "'$DATABASE'"' >> /etc/kong/kong.yml
 fi
 
+# Setting up the proper database
+if [ -n "$KONG_CONFIG" ]; then
+  echo -e $DATABASE|base64 --decode > /etc/kong/kong.yml
+fi
+
 exec "$@"
